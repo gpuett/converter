ee9a86f9e5afdf72ec31796b2e5df1e96b0be239
Garnett Puett and Kelsey Guerra <garnett@>
2018-03-21 13:16:43 -0700

    complete 4 converter forms.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,57 @@
-var gallons = parseInt(prompt("How many gallons?"));
+var liters1 = function(gallons) {
+  return gallons * 3.785;
+};
 
-var liters = function(gallons) { return gallons * 3.785 };
+var grams1 = function(oz) {
+  return oz * 28;
+};
 
-alert(liters(gallons) + " liters")
+var grams2 = function(pounds) {
+  return pounds * 454;
+};
+
+var liters2 = function(cups) {
+  return cups * 0.24;
+};
+
+
+
+$(document).ready(function(){
+  $("form#convert1").submit(function(event) {
+    event.preventDefault();
+    var gallons = parseInt($("#gallons").val());
+    var result = liters1(gallons);
+    $("#gallons-output").text(result);
+
+  });
+});
+
+$(document).ready(function(){
+  $("form#convert2").submit(function(event) {
+    event.preventDefault();
+    var oz = parseInt($("#oz").val());
+    var result = grams1(oz);
+    $("#oz-output").text(result);
+
+  });
+});
+
+$(document).ready(function(){
+  $("form#convert3").submit(function(event) {
+    event.preventDefault();
+    var pounds = parseInt($("#pounds").val());
+    var result = grams2(pounds);
+    $("#pounds-output").text(result);
+
+  });
+});
+
+$(document).ready(function(){
+  $("form#convert4").submit(function(event) {
+    event.preventDefault();
+    var cups = parseInt($("#cups").val());
+    var result = liters2(cups);
+    $("#cups-output").text(result);
+
+  });
+});
